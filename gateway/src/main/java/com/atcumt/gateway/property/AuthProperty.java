91cbe834ce0a848dde293149b74e6f9d58529b6f
Wotemo <wotemo@qq.com>
2024-11-03 00:35:47 +0800

    [chore]更改路径排除策略

--- gateway/src/main/java/com/atcumt/gateway/property/AuthProperty.java
+++ gateway/src/main/java/com/atcumt/gateway/property/AuthProperty.java
@@ -2,13 +2,14 @@ package com.atcumt.gateway.property;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 import java.util.List;
 
 @Data
-@ConfigurationProperties(prefix = "cumt-forum.auth")
+@ConfigurationProperties(prefix = "auth")
+@RefreshScope
 public class AuthProperty {
     private List<String> includePaths;
     private List<String> excludePaths;
 }
-
